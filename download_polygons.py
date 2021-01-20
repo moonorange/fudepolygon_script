04e49d640329d13d56b4aa5c24266c08ad89786c
moonorange <monoma632@gmail.com>
2021-01-20 18:19:53 +0900

    update: url format depending on fudepoly update

--- download_polygons.py
+++ download_polygons.py
@@ -12,17 +12,17 @@ import ipdb
 
 PREFECTURES = ['北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県', '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県', '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県', '静岡県', '愛知県', '三重県', '滋賀県', '京都府', '大阪府', '兵庫県', '奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県', '山口県', '徳島県', '香川県', '愛媛県', '高知県', '福岡県', '佐賀県', '長崎県', '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県']
 
-YEAR = '2020'
+YEAR = '（2020公開）'
 
 BASE_URL = 'http://www.machimura.maff.go.jp/polygon/'
 
-FUDEPOLYGONS_DIR = '../../auto_polygon_generator/src/'
+FUDEPOLYGONS_DIR = 'fudepolygon_data/'
 
 HOKKAIDO_DIR = FUDEPOLYGONS_DIR + '01北海道{}/'.format(YEAR)
 HOKKAIDO_GEO_CODES = ['11', '12', '13']
 
 def download_file(url: str) -> str:
-    filename = urllib.parse.unquote(url.split('/')[-1])
+    filename = FUDEPOLYGONS_DIR + urllib.parse.unquote(url.split('/')[-1])
     print('downloading {} ...'.format(filename))
     r = requests.get(url, stream=True)
     with open(filename, 'wb') as f:
@@ -48,6 +48,8 @@ def unzip_file(filename: str) -> None:
         zfile.extract(info, FUDEPOLYGONS_DIR)
 
 def download_fudepolygon_files() -> None:
+    if not os.path.isdir(FUDEPOLYGONS_DIR):
+        os.makedirs(FUDEPOLYGONS_DIR)
     for idx, pref in enumerate(PREFECTURES):
         encoded_pref = urllib.parse.quote(pref)
         pref_url = '{}{:02}{}{}.zip'.format(BASE_URL, idx + 1, encoded_pref, YEAR)
@@ -58,6 +60,8 @@ def download_fudepolygon_files() -> None:
 
 #　都道府県名を指定してファイルをダウンロードする関数
 def dwld_files_from_pref_num(pref_num) -> None:
+    if not os.path.isdir(FUDEPOLYGONS_DIR):
+        os.makedirs(FUDEPOLYGONS_DIR)
     for idx in pref_num:
         encoded_pref = urllib.parse.quote(PREFECTURES[idx - 1])
         pref_url = '{}{:02}{}{}.zip'.format(BASE_URL, idx, encoded_pref, YEAR)
