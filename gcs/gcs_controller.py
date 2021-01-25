41a1a0641e0a4d9bdcf9cd4559ad3e3fb9ef5672
moonorange <monoma632@gmail.com>
2021-01-25 11:22:30 +0900

    add: upload sorted fudepoly files

--- gcs/gcs_controller.py
+++ gcs/gcs_controller.py
@@ -14,15 +14,15 @@ class GcsController:
 		new_bucket = self.storage_cl.create_bucket(bucket, location="us-east1")
 		print("\n")
 		print(
-			"Created bucket {} in {} with storage class {}".format
+			"Created bucket {} in {} with storage class {}\n".format
 				(new_bucket.name, new_bucket.location, new_bucket.storage_class)
 		)
 		return new_bucket
 
 	def upload_data_to_bucket(self, bucket_name: str, source_dir: str = FUDEPOLYGONS_DIR):
 		bucket = self.storage_cl.bucket(bucket_name)
-		for path in glob.glob(source_dir + "*"):
-			import ipdb;ipdb.set_trace()
+		for path in sorted(glob.glob(source_dir + "*")):
 			blob = bucket.blob(path)
 			blob.upload_from_filename(path)
-			print("File {} uploaded to {}.".format(path, path))
+			print("File {} uploaded to {}.".format(path, bucket.name))
+		print("Finish uploading!")
