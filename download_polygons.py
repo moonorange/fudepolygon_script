d328ad259d1681efdb703c6069452ff789e21223
moonorange <monoma632@gmail.com>
2021-01-25 12:48:49 +0900

    add: code to handle when data exits

--- download_polygons.py
+++ download_polygons.py
@@ -37,10 +37,14 @@ def _rename(info: zipfile.ZipInfo) -> None:
     info.filename = info.filename.encode(encoding).decode('cp932')
 
 def unzip_file(filename: str) -> None:
+    unzipped_filename = os.path.splitext(filename)[0]
+    if os.path.exists(unzipped_filename):
+        print("{} is already unzipped\n".format(unzipped_filename))
     zfile = zipfile.ZipFile(filename)
     for info in zfile.infolist():
         _rename(info)
         zfile.extract(info, FUDEPOLYGONS_DIR)
+    print('unzipped {}'.format(filename))
 
 def download_fudepolygon_files(is_unzip: int) -> None:
     if not os.path.isdir(FUDEPOLYGONS_DIR):
@@ -51,7 +55,6 @@ def download_fudepolygon_files(is_unzip: int) -> None:
         file_name = download_file(pref_url)
         if (file_name and is_unzip):
             unzip_file(file_name)
-            print('unzipped {}'.format(file_name))
 
 #　都道府県名を指定してファイルをダウンロードする関数
 def dwld_files_from_pref_num(pref_num, is_unzip: int) -> None:
@@ -63,10 +66,9 @@ def dwld_files_from_pref_num(pref_num, is_unzip: int) -> None:
         file_name = download_file(pref_url)
         if (file_name and is_unzip):
             unzip_file(file_name)
-            print('unzipped {}'.format(file_name))
 
 def rm_zfiles() -> None:
-    for path in glob.glob('{}*.zip'.format(FUDEPOLYGONS_DIR)):
+    for path in glob.iglob('{}*.zip'.format(FUDEPOLYGONS_DIR)):
         os.remove(path)
 
 # 北海道の入れ子になっているzip fileをfudepolygons配下に解凍
@@ -93,15 +95,21 @@ if __name__ == "__main__":
     parser.add_argument('--rm', type=int, default=0)
     args = parser.parse_args()
 
+
     if (args.mode == "all"):
+        # 全都道府県
         download_fudepolygon_files(args.unzip)
     if (args.pref_num):
         # 都道府県名を指定してファイルをダウンロードしたい時
         dwld_files_from_pref_num(args.pref_num)
     if (args.gcs):
+        # gcsにアップロード
         strage_cl = storage.Client()
         gcs = GcsController(strage_cl)
-        bucket = gcs.create_bucket("COLDLINE", "us-east-1")
-        gcs.upload_data_to_bucket("fudepolygon_2020")
+        storage_class = "COLDLINE"
+        location = "us-east-1"
+        bucket = gcs.create_bucket(storage_class, location)
+        gcs.upload_data_to_bucket(bucket.name)
     if (args.rm):
+        # zipfileを消す
         rm_zfiles()
