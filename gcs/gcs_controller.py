e816f61f3e397e52605637986967d2e05f5c8e8a
moonorange <monoma632@gmail.com>
2021-01-25 11:10:30 +0900

    add: create bucket

--- gcs/gcs_controller.py
+++ gcs/gcs_controller.py
@@ -12,6 +12,7 @@ class GcsController:
 		bucket = self.storage_cl.bucket(bucket_name)
 		bucket.storage_class = storage_class
 		new_bucket = self.storage_cl.create_bucket(bucket, location="us-east1")
+		print("\n")
 		print(
 			"Created bucket {} in {} with storage class {}".format
 				(new_bucket.name, new_bucket.location, new_bucket.storage_class)
@@ -20,7 +21,6 @@ class GcsController:
 
 	def upload_data_to_bucket(self, bucket_name: str, source_dir: str = FUDEPOLYGONS_DIR):
 		bucket = self.storage_cl.bucket(bucket_name)
-		import ipdb;ipdb.set_trace()
 		for path in glob.glob(source_dir + "*"):
 			import ipdb;ipdb.set_trace()
 			blob = bucket.blob(path)
