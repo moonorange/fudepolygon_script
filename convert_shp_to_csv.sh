506b8a8b1949858962a28574bcc8c18708adadfb
moonorange <monoma632@gmail.com>
2021-01-26 17:47:04 +0900

    add: rename filename in bulk

--- convert_shp_to_csv.sh
+++ convert_shp_to_csv.sh
@@ -1,13 +1,2 @@
-fudepoly_path="fudepolygon_data/*"
-dirs=`find $fudepoly_path -name *.shp`
-
-i=0
-for path in $dirs;
-do
-	nonum=`echo $path | tr -d [0-9]`
-	`mv $path $nonum`
-	filename=`basename $path .shp`
-	echo $filename
-	# ogr2ogr -f csv -dialect sqlite -sql "select AsGeoJSON(geometry, 4326) AS geom, * from $filename" $path.csv $path
-	# let i++
-done
+filename=`basename $path .shp`
+ogr2ogr -f csv -dialect sqlite -sql "select AsGeoJSON(geometry, 4326) AS geom, * from $filename" $path.csv $path
---
+++ remove_num_from_filename.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+fudepoly_path="fudepolygon_data/*"
+dirs=`find $fudepoly_path -name *.shp -o -name *.dbf -o -name *.prj -o -name *.shx`
+
+i=0
+for path in $dirs;
+do
+	filename=`basename $path`
+	dirname=`dirname $path`
+	nonum=$(echo $filename | tr -d [0-9])
+	newpath="${dirname}/${nonum}"
+	mv $path "$newpath"
+	echo $newpath
+	let i++
+done
