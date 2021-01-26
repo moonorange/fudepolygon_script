86abb729c4731b427ae81c9dace12ec98b912b4d
moonorange <monoma632@gmail.com>
2021-01-26 18:51:47 +0900

    add: echo file number

--- convert_shp_to_csv.sh
+++ convert_shp_to_csv.sh
@@ -2,6 +2,7 @@
 
 fudepoly_path="fudepolygon_data/*"
 dirs=`find $fudepoly_path -name *.shp`
+total_num=`find . -name *.shp | wc -l`
 
 i=0
 for path in $dirs;
@@ -9,7 +10,7 @@ do
 	filename=`basename $path .shp`
 	ogr2ogr -f csv -dialect sqlite -sql "select AsGeoJSON(geometry, 4326) AS geom, * from $filename" "csv/$filename.csv" $path
 	let i++
-	echo $filename
+	echo "$filename $i
 done
 
 echo "finish converting $i files"
